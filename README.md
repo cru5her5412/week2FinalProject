c18c0322ca2b098fc17894d6abb94bd5c150da9e
Callum James Lodge <callumjlodge@gmail.com>
2025-08-22 13:47:52 +0100

    added webp format images of old images, fixed moving to bottom to function correctly, added intial creation of background image when page loads

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# week2FinalProject
-Week 2 final project
+##Requirements, Stretch goals and review
+#Requirements
+-🎯 Implement responsive design methods to ensure the website works well on both small mobile screens and larger desktop screens (e.g., above 800px).
+-🎯 Implement at least one meaningful media query so that there is a noticeable change between mobile and desktop view (an example is to change where the ‘thumbnail bar’ is positioned).
+-🎯 Ensure all images have appropriate alt text attributes for accessibility.
+-🎯 Correctly use event handlers to switch images based on user interactions.
+#Stretch Goals
+-🏹 Use ‘srcset’ to specify which image will be used based on the size of the screen for optimal viewing experience.
+-🏹 Add ARIA elements to improve accessibility, such as aria-label, aria-live, and other relevant attributes.
+-🏹 Implement key bindings for buttons to enhance navigation, such as using arrow keys to switch between images.
+#Review
--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 //Store image data
 let images = {
   sourceURL: [
-    "./images/MobieandSmutt.jpg",
-    "./images/BluandRocky.jpg",
-    "./images/OliandDewey.jpg",
+    "./images/MobieandSmutt.webp",
+    "./images/BluandRocky.webp",
+    "./images/OliandDewey.webp",
+    "./images/Mobie1.webp",
+    "./images/Blu1.webp",
+    "./images/Smutt1.webp",
+    "./images/Mobie2.webp",
+    "./images/Mobie3.webp",
+    "./images/Mobie4.webp",
   ],
   altText: [
     "A yellow labrador called Mobie, and a black cat called Smutt",
     "A grey cat called Blu and a black cat called Rocky",
     "A grey chinchilla called Oli and a white chinchilla called Dewey",
+    "A yellow labrador called Mobie, hiding her eyes from the light because she is trying to sleep",
+    "A grey cat called Blu, sprawled out on his favourite blanket",
+    "A black cat called smutt, licking his feet while sat on a chair",
+    "A yellow labrador called Mobie, snoozing, leaning her head on a desk",
+    "A yellow labrador called Mobie, curled up, squished into a chair",
+    "A yellow labrador called Mobie, sprawled on a sofa wearing some dog clothing",
   ],
 };
 
@@ -19,6 +31,9 @@ function createThumbnails() {
     newImg.src = images.sourceURL[i];
     newImg.alt = images.altText[i];
     newImg.className = "thumbnail-image";
+    if (i === 0) {
+      newImg.className = "thumbnail-image first-image";
+    }
     thumbnailContainer = document.querySelector(".thumbnail-container");
     thumbnailContainer.appendChild(newImg);
     newImg.addEventListener("click", function (event) {
@@ -30,7 +45,8 @@ function createThumbnails() {
 createThumbnails();
 
 //create main/background image
-
+const firstImage = document.querySelector(".first-image");
+createFullScreenImages(firstImage);
 function createFullScreenImages(image) {
   //remove old image
   mainImage = document.querySelector(".main-image-container");
